15bf4bd3da40c63d5030ea3e10240bbd83452d97
Raleigh.j55@gmail.com <r.aleigh.j55@gmail.com>
2021-06-29 18:53:36 -0500

    Updated

--- React/React_2/react.js
+++ React/React_2/react.js
@@ -1,17 +1,7 @@
-document.body.classList.add('fade-out');
-
-window.addEventListener('DOMContentLoaded', () => {
-    document.body.classList.remove('fade-out');
-})
-
-function validateForm() {
-    let x = document.forms["myForm"]["fname"]["lname"].value;
-    if (x == "") {
-        alert("Name must be filled out to submit");
-        return false;
-    }
-    else (x !== "") 
-        alert("Thank you for submitting")
-        return true;
-    } 
-
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,12 +6,30 @@ function App() {
   <script>
     document.body.classList.add('fade-out')
 
-    window.addEventListener('DOMContentLoaded', () => {
+    window.addEventListener('DOMContentLoaded', () {
     document.body.classList.remove('fade-out')
 })
 </script>
     </div>
   );
 }
+document.body.classList.add('fade-out');
+
+window.addEventListener('DOMContentLoaded', () => {
+    document.body.classList.remove('fade-out');
+})
+
+function validateForm() {
+    let x = document.forms["myForm"]["fname"]["lname"].value;
+    if (x == "") {
+        alert("Name must be filled out to submit");
+        return false;
+    }
+    else (x !== "") 
+        alert("Thank you for submitting")
+        return true;
+    } 
+
+
 
 export default App;
